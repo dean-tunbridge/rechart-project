6a59cea7643e0715ca3b2545dbb13ac064db475d
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 17:40:10 +1200

    make charts appear on column regardless of screen width

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10">
-        <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <div className="grid  w-full gap-10 max-w-[1400px]">
           <GridItem title="Area Chart">
             <AreaChartComponent />
           </GridItem>
