4ece1059322ac64a978f3eb8a69ba92280e2aec0
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 16:54:24 +1200

    create custom tooltip  component

---
+++ src/components/CustomToolTip.jsx
@@ -0,0 +1,17 @@
+export default function CustomToolTip({ active, payload, label }) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
+        <p className="text-medium text-lg">{label}</p>
+        <p className="text-sm text-purple-400">
+          Premier League Goals:
+          <span className="ml-2">${payload[0].value}</span>
+        </p>
+        <p className="text-sm text-red-400">
+          La Liga Goals:
+          <span className="ml-2">${payload[1].value}</span>
+        </p>
+      </div>
+    )
+  }
+}
