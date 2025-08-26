13acc0e90ac01a1f651933985af796c5abf79dcd
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 17:04:16 +1200

    complete outline of legend

--- src/components/CustomLegend.jsx
+++ src/components/CustomLegend.jsx
@@ -4,5 +4,19 @@ export default function CustomLegend({ payload }) {
     la_liga_goals: 'La Liga Goals',
   }
 
- return ()
+  if (payload)
+    return (
+      <ul className="flex gap-6 mt-4 justify-center">
+        {payload.map((entry, index) => (
+          <li key={index} className="flex items-center gap-2">
+            <span
+              className="w-3 h-3 rounded-full"
+              style={{ backgroundColor: entry.color }}></span>
+            <span className="text-sm text-white">
+              {nameKeyValues[entry.dataKey] || entry.dataKey}
+            </span>
+          </li>
+        ))}
+      </ul>
+    )
 }
