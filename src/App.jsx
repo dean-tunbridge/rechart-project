e1fd855b2c6c4175adb2e93d1e6898cf7f1da285
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 16:30:59 +1200

    fix issue with import of each component within app. Everything is working as it should now

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
       <div className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10">
         <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
           <GridItem title="Area Chart">
-            <AreaChart />
+            <AreaChartComponent />
           </GridItem>
           <GridItem title="Bar Chart">
-            <BarChart />
+            <BarChartComponent />
           </GridItem>
           <GridItem title="Line Chart">
-            <LineChart />
+            <LineChartComponent />
           </GridItem>
         </div>
       </div>
