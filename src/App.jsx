94878780139894425b2cf9f95cd72f83701b4723
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 15:55:09 +1200

    create grid component to hold charts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <div></div>
+      <div className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10">
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+          <GridItem title="Area Chart"></GridItem>
+          <GridItem title="Bar Chart"></GridItem>
+          <GridItem title="Line Chart"></GridItem>
+        </div>
+      </div>
     </>
   )
 }
