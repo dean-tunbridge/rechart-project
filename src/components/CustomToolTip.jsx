237a19fca7fad5b2457562137a46119f7bebad91
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 17:15:47 +1200

    add extra leagues to custom tool tip

--- src/components/CustomToolTip.jsx
+++ src/components/CustomToolTip.jsx
@@ -3,13 +3,28 @@ export default function CustomToolTip({ active, payload, label }) {
     return (
       <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
         <p className="text-medium text-lg">{label}</p>
-        <p className="text-sm text-purple-400">
+
+        <p className="text-sm text-text-[#FF268C]">
           Premier League Goals:
           <span className="ml-2">{payload[0].value}</span>
         </p>
-        <p className="text-sm text-red-400">
+        <p className="text-sm text-text-[#FFD500]">
           La Liga Goals:
           <span className="ml-2">{payload[1].value}</span>
+        </p>
+        <p className="text-sm text-[#E2001A]">
+          Bundesliga Goals:
+          <span className="ml-2">{payload[2].value}</span>
+        </p>
+
+        <p className="text-sm text-[#0066B2]">
+          Serie A Goals:
+          <span className="ml-2">{payload[3].value}</span>
+        </p>
+
+        <p className="text-sm text-[#003399]">
+          Ligue 1 Goals:
+          <span className="ml-2">{payload[4].value}</span>
         </p>
       </div>
     )
