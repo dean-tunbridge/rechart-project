3b8bfb55b4495e7e5477b26b7f3c7d3515cc577b
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-26 16:06:04 +1200

    import area chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import GridItem from './components/GridItem'
+import AreaChart from './components/AreaChart'
 
 function App() {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10">
         <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
-          <GridItem title="Area Chart"></GridItem>
+          <GridItem title="Area Chart">
+            <AreaChart />
+          </GridItem>
           <GridItem title="Bar Chart"></GridItem>
           <GridItem title="Line Chart"></GridItem>
         </div>
